2f78f72fc7bd900bdbcf6235e3bcdfc42e4079bd
Draga Doncila <ddon0001@student.monash.edu>
2024-02-13 15:54:36 +1100

    Add code to save out experiment results and config

--- experiment_schema.py
+++ experiment_schema.py
@@ -1,9 +1,13 @@
 from enum import Enum, auto
+import json
+import time
 import pandas as pd
 from pydantic import BaseModel, Field, conlist
 from tracktour import Tracker
-from typing import Optional, Union, List
+from typing import Optional, Union
 import os
+
+import yaml
 
 PathLike = Union[str, bytes, os.PathLike]
 
@@ -16,7 +20,7 @@ class TraxData(BaseModel):
     detections_path: PathLike
     frame_shape: conlist(int, min_length=2, max_length=3)
 
-    # Default values
+    # Default values available
     dataset_name: str = 'cells'
     location_keys: conlist(str, min_length=2, max_length=3) = ['y', 'x']
     frame_key: str = 't'
@@ -29,9 +33,6 @@ class TraxInstance(BaseModel):
     # migration_only: bool = False
     k: int = 10
 
-# class TraxTracker(BaseModel):
-#     pass
-    
 class Cost(Enum):
     INTERCHILD_DISTANCE = auto()
 
@@ -46,7 +47,6 @@ class TraxTour(BaseModel):
 class Traxperiment(BaseModel):
     data_config: TraxData
     instance_config: TraxInstance = Field(default_factory=TraxInstance)
-    # tracker_config: TraxTracker
     tracktour_config: TraxTour = Field(default_factory=TraxTour)
 
     def as_tracker(self):
@@ -54,6 +54,7 @@ class Traxperiment(BaseModel):
             im_shape=tuple(self.data_config.frame_shape), 
             k_neighbours=self.instance_config.k
         )
+        tracker.DEBUG_MODE = True
 
         # configure merge capacity
         tracker.MERGE_EDGE_CAPACITY = self.tracktour_config.merge_capacity
@@ -66,22 +67,59 @@ class Traxperiment(BaseModel):
         return tracker
 
     def run(self):
+        start = time.time()
         tracker = self.as_tracker()
         
         # load detections
         detections = pd.read_csv(self.data_config.detections_path)
         
-        migration_edges = tracker.solve(
+        # solve model
+        tracked = tracker.solve(
             detections=detections,
             frame_key=self.data_config.frame_key,
             location_keys=self.data_config.location_keys
         )
-        return migration_edges
+
+        # write everything out (including tracktour commit...)
+        out_root = self.data_config.out_root_path
+        # TODO: formalize/document that we're writing out and potentially overwriting path
+        out_ds = os.path.join(out_root, self.data_config.dataset_name)
+        os.makedirs(out_ds, exist_ok=True)
+        
+        # write out dataframes
+        tracked_dict = dict(tracked)
+        for k in ['tracked_edges', 'tracked_detections', 'all_edges', 'all_vertices']:
+            out_pth = os.path.join(out_ds, f'{k}.csv')
+            if (info := tracked_dict.pop(k)) is not None:
+                info.to_csv(out_pth, index=False)
+
+
+        # write out model (mps and lp)
+        if (model := tracked_dict.pop('model')) is not None:
+            model.write(os.path.join(out_ds, 'model.lp'))
+            model.write(os.path.join(out_ds, 'model.mps'))
+        
+        # write tracktour version
+
+        # write config (yaml ideally)
+        with open(os.path.join(out_ds, 'config.yaml'), 'w') as f:
+            yaml.dump(self.model_dump(), f)
+
+        # experiment wall time
+        duration = time.time() - start
+        tracked_dict.update({
+            'exp_time': duration
+        })
+        with open(os.path.join(out_ds, 'times.json'), 'w') as f:
+            json.dump(tracked_dict, f)
+
+
+        return tracked
 
 if __name__ == '__main__':
     DATA_ROOT = '/home/ddon0001/PhD/data/cell_tracking_challenge/ST/Fluo-N2DL-HeLa/'
     data_config = TraxData(
-        dataset_name='Fluo-N2DL-HeLa_1',
+        dataset_name='Fluo-N2DL-HeLa_01',
         image_path=os.path.join(DATA_ROOT, '01/'),
         segmentation_path=os.path.join(DATA_ROOT, '01_ST/SEG/'),
         detections_path='/home/ddon0001/PhD/data/cell_tracking_challenge/ST/Fluo-N2DL-HeLa/01_ST/SEG/detections.csv',
@@ -89,7 +127,8 @@ if __name__ == '__main__':
         frame_shape=[700, 1100]
     )
     experiment = Traxperiment(data_config=data_config)
+    experiment.tracktour_config.appearance_cheat = True
     
-    print(experiment.model_dump_json(indent=2))
-    migration_edges = experiment.run()
-    print(migration_edges.head())
+    tracked = experiment.run()
+    migration_edges = tracked.tracked_edges
+    tracked_detections = tracked.tracked_detections
