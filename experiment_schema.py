f33a96473a1278edc8a605a4bd19e37d72bb469e
Draga Doncila <ddon0001@student.monash.edu>
2024-02-15 11:46:55 +1100

    Parameterize outfile split writing

--- experiment_schema.py
+++ experiment_schema.py
@@ -12,6 +12,27 @@ import yaml
 
 PathLike = Union[str, bytes, os.PathLike]
 
+class OUT_FILE(Enum):
+    # solutions
+    TRACKED_DETECTIONS = 'tracked_detections.csv'
+    TRACKED_EDGES = 'tracked_edges.csv'
+    ALL_VERTICES = 'all_vertices.csv'
+    ALL_EDGES = 'all_edges.csv'
+
+    # models
+    MODEL_LP = 'model.lp'
+    MODEL_MPS = 'model.mps'
+
+    # tracktour version
+    TRACKTOUR_VERSION = 'version.txt'
+    
+    # timings
+    TIMING = 'times.json'
+
+    # config
+    CONFIG = 'config.yaml'
+
+
 class TraxData(BaseModel):
     # these should also be optional, we only need them for metrics
     # maybe we should have a separate metrics config
@@ -81,6 +102,11 @@ class Traxperiment(BaseModel):
             location_keys=self.data_config.location_keys
         )
 
+        self.write_solved(tracked, start)
+
+        return tracked
+    
+    def write_solved(self, tracked, start):
         # write everything out (including tracktour commit...)
         out_root = self.data_config.out_root_path
         # TODO: formalize/document that we're writing out and potentially overwriting path
@@ -89,26 +115,27 @@ class Traxperiment(BaseModel):
         
         # write out dataframes
         tracked_dict = dict(tracked)
-        for k in ['tracked_edges', 'tracked_detections', 'all_edges', 'all_vertices']:
-            out_pth = os.path.join(out_ds, f'{k}.csv')
-            if (info := tracked_dict.pop(k)) is not None:
+        for k in [OUT_FILE.TRACKED_EDGES, OUT_FILE.TRACKED_DETECTIONS, OUT_FILE.ALL_EDGES, OUT_FILE.ALL_VERTICES]:
+            file_key = k.value
+            attr_key = file_key.split('.')[0]
+            out_pth = self.get_path_to_out_file(k)
+            if (info := tracked_dict.pop(attr_key)) is not None:
                 info.to_csv(out_pth, index=False)
-
 
         # write out model (mps and lp)
         if (model := tracked_dict.pop('model')) is not None:
-            model.write(os.path.join(out_ds, 'model.lp'))
-            model.write(os.path.join(out_ds, 'model.mps'))
+            model.write(self.get_path_to_out_file(OUT_FILE.MODEL_LP))
+            model.write(self.get_path_to_out_file(OUT_FILE.MODEL_MPS))
         
         # TODO: formalize/document that the BUILT version is written, so we don't want to
         # be running experiments in editable mode
         # write tracktour version
         tracktour_version = version(distribution_name='tracktour')
-        with open(os.path.join(out_ds, 'version.txt'), 'w') as f:
+        with open(self.get_path_to_out_file(OUT_FILE.TRACKTOUR_VERSION), 'w') as f:
             f.write(tracktour_version)
 
         # write config (yaml ideally)
-        with open(os.path.join(out_ds, 'config.yaml'), 'w') as f:
+        with open(self.get_path_to_out_file(OUT_FILE.CONFIG), 'w') as f:
             yaml.dump(self.model_dump(), f)
 
         # experiment wall time
@@ -116,11 +143,12 @@ class Traxperiment(BaseModel):
         tracked_dict.update({
             'exp_time': duration
         })
-        with open(os.path.join(out_ds, 'times.json'), 'w') as f:
+        with open(self.get_path_to_out_file(OUT_FILE.TIMING), 'w') as f:
             json.dump(tracked_dict, f)
-
-
-        return tracked
+    
+    def get_path_to_out_file(self, file_key: OUT_FILE):
+        out_ds = os.path.join(self.data_config.out_root_path, self.data_config.dataset_name)
+        return os.path.join(out_ds, file_key.value)
 
 if __name__ == '__main__':
     DATA_ROOT = '/home/ddon0001/PhD/data/cell_tracking_challenge/ST/Fluo-N2DL-HeLa/'
